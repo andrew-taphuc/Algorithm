f91e19dded7171aa673daa8e3f8eb8d2b24d1d6b
Ta Hong Phuc <140427335+andrew-taphuc@users.noreply.github.com>
2024-06-24 10:55:52 +0700

    Add files via upload

---
+++ code-force/A_1371.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+    int t = 0;
+    scanf("%d \n", &t);
+while (t--)
+{
+    int n = 0;
+    scanf("%d", &n);
+    printf("%d \n", (n+1)/2);
+}
+return 0;
+}

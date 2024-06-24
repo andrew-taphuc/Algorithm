9098501650a002ac86fe003ccac22d87a4b0a48e
Ta Hong Phuc <140427335+andrew-taphuc@users.noreply.github.com>
2024-06-24 10:56:43 +0700

    Add files via upload

---
+++ code-force/tempCodeRunnerFile.c
@@ -0,0 +1,8 @@
+    else if (x == b)
+    printf("%lld%lld%lld", x - a, x - c, x - d);
+
+    else if (x == c)
+    printf("%lld%lld%lld", x - b, x - a, x - d);
+
+    else
+    printf("%lld%lld%lld", x - b, x - c, x - a);
